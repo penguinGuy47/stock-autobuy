7b4b0bc4b550da6bd16b19e66fd8044f1690a415
Kaleb <kaleblianggg@icloud.com>
2024-09-01 01:08:56 -0500

    added ability to sell specified number of shares

--- fidelity.py
+++ fidelity.py
@@ -1,5 +1,9 @@
 from sleep import *
 
+# ENTER YOUR CREDENTIALS
+username = ""   # ENTER YOUR USERNAME
+pw = ""         # ENTER YOUR PASSWORD
+
 # TODO:
 # add multi buy function
 def buy(ticker, dir, prof):
@@ -12,11 +16,6 @@ def buy(ticker, dir, prof):
     driver.get("https://digital.fidelity.com/ftgw/digital/portfolio/summary")
     wait = WebDriverWait(driver, 10)
     short_sleep()
-
-    # ENTER YOUR CREDENTIALS
-    username = ""   # ENTER YOUR USERNAME
-    pw = ""         # ENTER YOUR PASSWORD
-    
 
     username_field = driver.find_element(By.XPATH, '//*[@id="dom-username-input"]')
     username_field.click()
@@ -38,15 +37,12 @@ def buy(ticker, dir, prof):
     # click trade
     trade_button = driver.find_element(By.XPATH, '//*[@id="action-bar--container"]/div[2]/div[1]/ul/li[1]/a')
     trade_button.click()
-
     short_sleep()
 
     # click dropdown
     account_dropdown = driver.find_element(By.XPATH, '//*[@id="dest-acct-dropdown"]')
     account_dropdown.click()
-
     very_short_sleep()
-    print("clicked dropdown")
 
     account_select = driver.find_element(By.XPATH, '//*[@id="ett-acct-sel-list"]')
     very_short_sleep()
@@ -123,6 +119,17 @@ def buy(ticker, dir, prof):
     driver.quit()
 
 def sell(ticker, dir, prof):
+    while True:
+        sell_share_count = input("How many shares would you like to sell? For all shares, type 'all', else enter an amount: ")
+        
+        if sell_share_count.lower() == "all":
+            break
+        elif re.fullmatch(r'\d+', sell_share_count):
+            break
+        else:
+            print("Invalid input. Please enter 'all' or a number.")
+        
+
     options = webdriver.ChromeOptions()
     options.add_argument(f"user-data-dir={dir}")
     options.add_argument(f"--profile-directory={prof}")
@@ -131,14 +138,7 @@ def sell(ticker, dir, prof):
     # driver.get("https://digital.fidelity.com/prgw/digital/login/full-page")
     driver.get("https://digital.fidelity.com/ftgw/digital/portfolio/summary")
     wait = WebDriverWait(driver, 10)
-    short_sleep()
-
-    # ENTER YOUR CREDENTIALS
-    # username = "minc8088"   # ENTER YOUR USERNAME
-    # pw = "Jcpledd123?"         # ENTER YOUR PASSWORD
-
-    username = "ying8088"   # ENTER YOUR USERNAME
-    pw = "Jcpledd123?"    
+    short_sleep() 
 
     username_field = driver.find_element(By.XPATH, '//*[@id="dom-username-input"]')
     username_field.click()
@@ -160,15 +160,12 @@ def sell(ticker, dir, prof):
     # click trade
     trade_button = driver.find_element(By.XPATH, '//*[@id="action-bar--container"]/div[2]/div[1]/ul/li[1]/a')
     trade_button.click()
-
     short_sleep()
 
     # click dropdown
     account_dropdown = driver.find_element(By.XPATH, '//*[@id="dest-acct-dropdown"]')
     account_dropdown.click()
-
     very_short_sleep()
-    print("clicked dropdown")
 
     account_select = driver.find_element(By.XPATH, '//*[@id="ett-acct-sel-list"]')
     very_short_sleep()
@@ -203,9 +200,12 @@ def sell(ticker, dir, prof):
         # enter quantity
         qty = driver.find_element(By.XPATH, '//*[@id="eqt-shared-quantity"]')
         qty.click()
-        time.sleep(10000)
-        # try to get div to expand!!!
-        qty.send_keys("1")
+        very_short_sleep()
+        if (sell_share_count == "all"):   # sell all
+            sell_all_button = driver.find_element(By.XPATH, '//*[@id="stock-quatity"]/div/div[2]/div/pvd3-button')
+            sell_all_button.click()
+        else:   # sell user specified
+            human_type(sell_share_count, qty)
         very_short_sleep()
 
         # click somewhere else
@@ -213,26 +213,26 @@ def sell(ticker, dir, prof):
         price_area.click()
         very_short_sleep()
 
-        # click limit buy
-        limit_buy_button = driver.find_element(By.XPATH, '//*[@id="market-no"]/s-root/div/label')
-        limit_buy_button.click()
+        # click market sell
+        market_sell_button = driver.find_element(By.XPATH, '//*[@id="market-yes"]/s-root/div/label')
+        market_sell_button.click()
         very_short_sleep()
 
-        # get current price
-        current_price = driver.execute_script('return document.querySelector("#eq-ticket__last-price .last-price").textContent')
-        current_price = float(current_price[1:]) # removes '$' in front
-        current_price += 0.1
-        current_price = f"{current_price:.2f}"
-
+        # get current price for limit order
+        # current_price = driver.execute_script('return document.querySelector("#eq-ticket__last-price .last-price").textContent')
+        # current_price = float(current_price[1:]) # removes '$' in front
+        # current_price += 0.1
+        # current_price = f"{current_price:.2f}"
         # enter limit price
-        limit_price = driver.find_element(By.XPATH, '//*[@id="eqt-ordsel-limit-price-field"]')
-        human_type(current_price, limit_price)
-        very_short_sleep()
+        # limit_price = driver.find_element(By.XPATH, '//*[@id="eqt-ordsel-limit-price-field"]')
+        # human_type(current_price, limit_price)
+        # very_short_sleep()
 
         # preview button
         preview_button = driver.find_element(By.XPATH, '//*[@id="previewOrderBtn"]/s-root/button')
         preview_button.click()
         very_short_sleep()
+        time.sleep(10000)
 
         # submit
         submit_button = driver.find_element(By.XPATH, '//*[@id="placeOrderBtn"]')
