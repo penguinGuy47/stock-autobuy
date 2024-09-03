72ee66a4b7fd4d1f8f8328a913256550381788a4
Kaleb <kaleblianggg@icloud.com>
2024-09-02 23:29:45 -0500

    updated function calls

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ import wellsfargo
 
 def main():
     # ticker = input("Enter the ticker you would to purchase: ")
-    ticker = 'AMC'
+    ticker = 'COIN'
     chrome_path = r"C:\Users\YourUserName\AppData\Local\Google\Chrome\User Data" # add the path to your chrome
-    chrome_profile = r"Profile 6" # change this accordingly
+    chrome_profile = r"Profile 5" # change this accordingly
+    # sell_share_count = input("How many shares would you like to sell? For all shares, type 'all', else enter an amount: ")
 
     # chase.buy(ticker)
     # tradier.buy(ticker)
-    # fidelity.buy(ticker)
+    fidelity.buy(ticker, chrome_path, chrome_profile)
+    # fidelity.sell(ticker, chrome_path, chrome_profile)
     # firstrade.buy(ticker, chrome_path, chrome_profile)
-    schwab.buy(ticker, chrome_path, chrome_profile)
+    # schwab.buy(ticker, chrome_path, chrome_profile)
+    # sofi.buy(ticker, chrome_path, chrome_profile)
 
 if __name__ == "__main__":
     main()
