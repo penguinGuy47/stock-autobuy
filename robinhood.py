352a42bcf9dbdfb45ef03bb47625fafe07dea117
Kaleb <kaleblianggg@icloud.com>
2024-09-07 21:02:09 -0500

    robinhood buy/sell available, need to add additional error handling and cookie saving (not priority)

--- robinhood.py
+++ robinhood.py
@@ -10,7 +10,7 @@ pw = ""         # ENTER YOUR PASSWORD
 # add account switching
 # add multi buy function
 def buy(ticker, dir, prof, qty):
-    driver = start_regular_driver(dir, prof)
+    driver = start_headless_driver(dir, prof)
     # driver = start_headless_driver(dir, prof)
     driver.get("https://robinhood.com/us/en/")
     short_sleep()
@@ -25,13 +25,17 @@ def buy(ticker, dir, prof, qty):
     short_sleep()
     login(driver)
     setup_trade(driver, ticker)
-    enter_share_qty(driver, qty)
-    submit_order(driver)
-    get_num_accounts()
+    accounts = get_num_accounts(driver)
+    for accNum in range(1, accounts+1):
+        if accNum != 1:
+            switch_accounts(driver, accNum)
+        enter_share_qty(driver, qty)
+        submit_order(driver)
+    driver.quit()
 
 
 def sell(ticker, dir, prof, qty):
-    driver = start_headless_driver(dir, prof)
+    driver = start_regular_driver(dir, prof)
     driver.get("https://robinhood.com/login/")
     short_sleep()
     login(driver)
@@ -42,8 +46,13 @@ def sell(ticker, dir, prof, qty):
     enter_sell.click()
 
     very_short_sleep()
-    enter_share_qty(driver, qty)
-    submit_order(driver)
+    accounts = get_num_accounts(driver)
+    for accNum in range(1, accounts+1):
+        if accNum != 1:
+            switch_accounts(driver, accNum)
+        enter_share_qty(driver, qty)
+        submit_order(driver)
+    driver.quit()
 
 
 def login(driver):
@@ -103,25 +112,42 @@ def login(driver):
 
 
 def setup_trade(driver, ticker):
-    # searches the ticker
-    ticker_search = driver.find_element(By.XPATH, '//*[@id="downshift-42-input"]')
-    ticker_search.click()
-    human_type(ticker, ticker_search)
-    very_short_sleep()
-    ticker_search.send_keys(Keys.ARROW_DOWN)
-    ticker_search.send_keys(Keys.ENTER)
-    very_short_sleep()
+    # searches for the ticker
+    try:
+        # short_sleep()
+        # ticker_search = driver.find_element(By.XPATH, '//*[@id="downshift-0-input"]')
+        ticker_search = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="downshift-0-input"]'))
+        )
+        very_short_sleep()
+        ticker_search.click()
+        human_type(ticker, ticker_search)
+        very_short_sleep()
+        ticker_search.send_keys(Keys.ARROW_DOWN)
+        ticker_search.send_keys(Keys.ENTER)
+        short_sleep()
+    except TimeoutException:
+        print("Timed out waiting ticker search to appear...")
 
 def enter_share_qty(driver, qty):
+    # select buy in shares
+    short_sleep()
+    try:
+        shares_dropdown = driver.find_element(By.XPATH, '//button[starts-with(@id, "downshift-") and contains(@id, "-toggle") and .//span[text()="Dollars"]]')
+        shares_dropdown.click()
+        short_sleep()
+        # disregard everything before: -options-menu-list-option-share"]
+        shares_button = driver.find_element(By.XPATH, '//*[contains(@id, "options-menu-list-option-share")]')
+        shares_button.click()
+        short_sleep()
+    except:
+        pass
+
+    random_click = driver.find_element(By.XPATH, '//*[@id="react_root"]/main/div[2]/div/div/div/div/main/div/div/div/header')
+    random_click.click()
+
+    print("Entering share qty...")
     # enter number of shares shares
-    shares_dropdown = driver.find_element(By.XPATH, '//*[@id="downshift-120-toggle-button"]')
-    shares_dropdown.click();
-    very_short_sleep()
-
-    shares_button = driver.find_element(By.XPATH, '//*[@id="af1eca935-ccf1-40f2-87c9-f9a7645b6d15-options-menu-list-option-share"]')
-    shares_button.click()
-    very_short_sleep()
-
     shares_qty = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/form/div[2]/div/div[3]/div/div/div/div/input')
     human_type(qty, shares_qty)
     very_short_sleep()
@@ -134,11 +160,12 @@ def submit_order(driver):
     verify_submit = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/form/div[3]/div/div[2]/div[1]/div/button')
     verify_submit.click()
     very_short_sleep()
-    done_button = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/div/div[3]/div[1]/button')
+    done_button = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/div/div[3]/div/button')
     done_button.click()
     short_sleep()
 
 def get_num_accounts(driver):
+    print("Getting all your accounts...")
     initial_dropdown = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/form/button')
     initial_dropdown.click()
     very_short_sleep()
@@ -146,12 +173,18 @@ def get_num_accounts(driver):
     div = driver.find_element(By.XPATH, '//*[@id="account-switcher-popover"]/div')
     accounts = div.find_elements(By.TAG_NAME, 'button')
     num_accounts = len(accounts)
-    # //*[@id="account-switcher-popover"]/div/button[1]
-    # //*[@id="account-switcher-popover"]/div/button[2]
 
     return num_accounts 
 
 def switch_accounts(driver, account_num):
+    # //*[@id="account-switcher-popover"]/div/button[1]
+    # //*[@id="account-switcher-popover"]/div/button[2]
+    initial_dropdown = driver.find_element(By.XPATH, '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/form/button')
+    initial_dropdown.click()
+    very_short_sleep()
+    next_account = driver.find_element(By.XPATH, f'//*[@id="account-switcher-popover"]/div/button[{account_num}]')
+    next_account.click()
+    short_sleep()
     pass
     
 
