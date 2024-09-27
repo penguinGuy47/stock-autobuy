f7e72f4b8ef15b798d7dd6899faf278f9d1fda4d
Kaleb <kaleblianggg@icloud.com>
2024-09-26 21:05:37 -0500

    removed unnecessary comments

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from controllers.trade_controller import trade_bp
 
-# Initialize the Flask app
 app = Flask(__name__)
-CORS(app) 
-
-# Register Blueprints
-app.register_blueprint(trade_bp, url_prefix='/api/trade')
-
-# optional health check
-# @app.route('/health', methods=['GET'])
-# def health_check():
-#     return jsonify({"status": "up"}), 200
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+app.register_blueprint(trade_bp, url_prefix='/')
 
 if __name__ == '__main__':
     app.run(debug=True)
