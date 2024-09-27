358872b371a99831d1a20446acd389a2048625ac
Kaleb <kaleblianggg@icloud.com>
2024-09-26 21:00:27 -0500

    removed unnecessary comments

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/trade', // Adjust the base URL as needed
+  baseURL: 'http://localhost:5000/',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Optional: Add interceptors if needed
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     // Handle errors globally if desired
-//     return Promise.reject(error);
-//   }
-// );
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Handle errors globally
+    return Promise.reject(error);
+  }
+);
 
 export default api;
