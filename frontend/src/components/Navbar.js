7196bfb2d0c7f525380b1ed8cf2fa44c1bad3f5a
Kaleb <kaleblianggg@icloud.com>
2024-09-26 20:56:02 -0500

    navbar, only has dashboard for now

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav style={styles.nav}>
-      <h2 style={styles.title}>Stock Trading App</h2>
+      <h2 style={styles.title}>Stock Trading App V0.1</h2>
       <ul style={styles.navLinks}>
         <li><Link to="/" style={styles.link}>Dashboard</Link></li>
-        {/* Add more links */}
       </ul>
     </nav>
   );
